434b35ace130066ab7cf1a0b80cc5c5f05857214
Roberta <robertha07rocha@gmail.com>
2022-01-24 19:19:43 -0300

    alteraçoes no projeto react,criado o dockerfile e o dockerignore

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import RobertaImg from './assets/roberta.png'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      
+        <p
+          
         >
-          Learn React
-        </a>
+          Olá, meu nome é Roberta, estou participando do desafio para o Estágio em DevOps na Frexco!
+          
+        </p>
+        <img src={RobertaImg} className='imgroberta'/>
+       
       </header>
     </div>
   );
